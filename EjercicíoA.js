f09897fbc41307f7b51201f1b2c35f6e676b73b0
brunonic17 <bscistri@gmail.com>
2023-08-21 22:32:17 -0300

    colocando bucle

---
+++ EjercicíoA.js
@@ -0,0 +1,12 @@
+let Elementos=[]
+const MC="Por favor, cargue un elemento"
+
+// Elementos.forEach(Elemento)
+
+
+for(let TE=Elementos.length;TE<=4;TE++){
+    let CE=prompt(MC) 
+    Elementos.push(CE)   
+ }
+
+console.log(Elementos)
