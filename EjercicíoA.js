421b057026806f4e20eca5b5be26d3f12bbc384f
brunonic17 <bscistri@gmail.com>
2023-08-23 22:36:10 -0300

    se termino la sumatoria

--- EjercicíoA.js
+++ EjercicíoA.js
@@ -1,14 +1,18 @@
-let Elementos=[]
+
+
+
+   
+    const Elementos=[]
 const MC="Por favor, cargue un elemento"
-
-// Elementos.forEach(Elemento)
-
-
 for(let TE=Elementos.length;TE<=4;TE++){
-    let CE=prompt(MC) 
-    Elementos.push(CE)   
+    let CE=prompt(MC);
+    CE= +CE;
+    Elementos.push(CE);
  }
 
-let Suma=Elementos[0]+Elementos[1]+Elementos[2]+Elementos[3]+Elementos[4]
-
-alert(Suma)
+    let sum=0
+    Elementos.forEach(Suma);
+    function Suma(element) {
+      sum += element;
+    }
+alert(sum)
