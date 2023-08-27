e0d52a399ff2c319b58e90204be395a2ecaadf36
brunonic17 <bscistri@gmail.com>
2023-08-26 22:42:45 -0300

    termine ciclo

--- EjercicíoB.js
+++ EjercicíoB.js
@@ -1,41 +1,33 @@
 let M="Menú"
 let Opciones=["1-Cargar Nombre y apellido del alumno","2-Cargar Notas","3-Sacar el promedio de las notas"]
-
+let cnaa=true
 Opciones.forEach((element)=>{
 M += "\n"+element })
-
-let OP=prompt(M)
-
+let Alumno =[]
+let nota =[]
+while(cnaa){let OP=prompt(M)
 switch(OP){
-  case "1": do{let CNA=["1-Cargar Datos","2-Finalizar Carga"]
-            let cna="Menú"
-            CNA.forEach((element)=>{cna += "\n"+element})
-            let CD=prompt(cna)
-            switch(CD){ 
-            case "1":  let Alumno =[]
-            var CA=prompt("Nombre y apellido del alumno")
-            Alumno.push(CA)
-             break
-            case "2":prompt(M)
-            break    }}while(CA!=null)
+  case "1": 
+  var CA=prompt("Nombre y apellido del alumno");
+  Alumno.push(CA);
+  console.log(Alumno)
   break
-  case "2":do{ let cn="Menú"
-            let CN=["1-Cargar Nota","2-Finalizar carga"]
-            CN.forEach((element)=>{cn += "\n"+element})
-            switch(CN){
-              case "1":   let nota =[]
-                          var N=prompt("Cargar nota")
-                          N= +N
-                          nota.push(N)
-              break
-              case "2":prompt(M)
-              break
-            }}while(N==Number)
-            break
+  case "2": 
+  var N=prompt("Cargar nota")
+// if(N!=Number||N!=null){
+//   let EN=prompt("Porfavor, coloque caracteres numericos")
+//   EN= +EN
+// nota.push(EN)
+// }
+// else{N= +N
+//   nota.push(N)
+// }
+// console.log(nota)
+break
 case"3": let sum=0
 nota.forEach(Suma)
 function Suma(element){sum += element}
-let cociente=nota.lenght
-function Promedio(Suma,cociente){Suma/cociente}
-alert("El promedio de"+" "+Alumno+" es igual "+Promedio)
-}
+let cociente=  nota.length
+alert("El promedio de"+" "+Alumno+" es igual "+sum/cociente)
+break
+}}
