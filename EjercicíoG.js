aff8146dc9754a74543f491dbef8cb5e52b9f42d
brunonic17 <scistrimauro123@gmail.com>
2023-09-16 22:29:51 -0300

    correcciones

--- EjercicíoG.js
+++ EjercicíoG.js
@@ -5,30 +5,25 @@
  let Elementoabuscar =""
  let mensajetrue="La letra esta dentro del grupo"
  let mensajefalse="La letra no esta dentro del grupo"
+ let mensajemenú= "Dado al azar, 10 letras del abecedario adivina una de este grupo \n 1-Coloque una letra del abecedario \n 2-Ver resultado"
+ let mensajeletra="Coloque la letra adivinar"
+ 
+ function busqueda(Elementoabuscar,Elementos){
+  Elementos.forEach((element)=>{
+    if(element==Elementoabuscar){ console.log(mensajetrue)}
+    else{console.log(mensajefalse) }})};
 
  while (true) {
-    let menú = ["Dado al azar, 10 letras del abecedario adivina una de este grupo \n 1-Coloque una letra del abecedario \n 2-Ver resultado"]
+    let menú = mensajemenú
     let selección = prompt(menú)
     switch (selección) {
-      
-      case "1": Elementoabuscar = prompt("Coloque la letra adivinar")
+      case "1": Elementoabuscar = prompt(mensajeletra)
       Elementoabuscar=Elementoabuscar.toUpperCase()
       console.log(Elementoabuscar)
         break
-      case "2": function busqueda(Elementoabuscar,Elementos){
-        Elementos.forEach((element)=>{
-          if(element=Elementoabuscar){ return mensajetrue}
-
-           })
-          
-           
-    //       else{alert(mensajefalse)}})
-    //   }
-        
-    
-    }
-    alert(busqueda(Elementoabuscar,Elementos))
-break
+      case "2":
+   console.log(busqueda(Elementoabuscar,Elementos))
+       break
   }
 
  }
