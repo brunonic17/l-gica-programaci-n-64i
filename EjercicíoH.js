a2853e4cfe4d48d037bcd5942540dc41a0266c2c
brunonic17 <scistrimauro123@gmail.com>
2023-09-17 13:22:31 -0300

    falta un pelin perro

--- EjercicíoH.js
+++ EjercicíoH.js
@@ -5,20 +5,36 @@
  let numero =""
  let mensajetrue="La letra esta dentro del grupo"
  let mensajefalse="La letra no esta dentro del grupo"
- let mensajemenú= "Por favor, colocar 5 numeros al azar \n 1-Coloque elemento \n 2-Ver el orden"
+ let mensajemenú= "Por favor, colocar 5 numeros al azar \n 1-Coloque elemento \n 2-Ver el orden ascendente \n 3-Ver el orden descendente "
  let mensajeelemet="Coloque un número"
  
- function busqueda(Elementoabuscar,Elementos){
-  Elementos.forEach((element)=>{
-    if(element==Elementoabuscar){alert(mensajetrue)}
-    else{alert(mensajefalse) }})};
+ function ordenarasc(Elementos){
+  for(let i=0;i<Elementos.length-1;i++){
+    for(let j=0;j<Elementos.length-1-i;j++){
+      if(Elementos[j]>Elementos[j+1]){
+        [Elementos[j],Elementos[j+1]]=[Elementos[j+1],Elementos[j]];
+          }
+        }
+      }
+      return alert(Elementos)
+    };
 
+    function ordenardesc(Elementos){
+      for(let i=0;i<Elementos.length-1;i++){
+        for(let j=0;j<Elementos.length-1-i;j++){
+          if(Elementos[j+1]>Elementos[j]){
+            [Elementos[j],Elementos[j+1]]=[Elementos[j+1],Elementos[j]];
+              }
+            }
+          }
+          return alert(Elementos)
+        };
+    
  while (true) {
-    let menú = mensajemenú
-    let selección = prompt(menú)
+    let selección = prompt(mensajemenú)
     switch (selección) {
       case "1": 
-      for(let i=0;Elementos.length()<5;i++){
+      for(let i=0;Elementos.length<5;i++){
         numero = prompt(mensajeelemet)
         numero= +numero
         Elementos.push(numero)}
@@ -26,7 +42,10 @@
       console.log(Elementos)
         break
       case "2":
-   alert(busqueda(Elementoabuscar,Elementos))
+   alert(ordenarasc(Elementos))
+       break
+       case"3":
+       alert(ordenardesc(Elementos))
        break
   }
 
